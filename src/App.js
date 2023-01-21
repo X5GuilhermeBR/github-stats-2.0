92957ec53d121f8669e24cb0c51cdc4b045fb5b3
Guilherme José <gui_guifonseca@hotmail.com>
2023-01-21 12:49:41 -0300

    Fix files with lint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
 
-function App() {
+function App () {
   return (
     <div className="App">
       <header className="App-header">
